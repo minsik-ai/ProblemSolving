7ee3b1aa4e0437c1ef38c960a753bb017643857b
Trent <devtrentoh@gmail.com>
2019-05-28 09:17:49 +0900

    Bacterial Tactics

--- CodeJam/2019/R1/C/BacterialTactics.py
+++ CodeJam/2019/R1/C/BacterialTactics.py
@@ -4,6 +4,17 @@ from operator import itemgetter
 import numpy as np
 
 
+def hash(matrix, r, c):
+    partial_strings = []
+    print(matrix)
+    for i in range(len(matrix)):
+        char_list = matrix[i].tolist()
+        print(char_list)
+        partial_strings.append(''.join(char_list))
+
+    return ''.join(partial_strings)
+
+
 def solve(inputs, r, c):
     # Get possible first moves
     # Recursively check for possible moves afterward that they are winning moves.
@@ -32,19 +43,28 @@ def solve(inputs, r, c):
         if '.' not in new_inputs:
             return False
 
-        partial_row_filled_states = [fill_partial_rows(new_inputs, i) for i in range(r)]
-        partial_col_filled_states = [fill_partial_cols(new_inputs, i) for i in range(c)]
+        print(r)
 
-        all_possible_states_winning_result = []
-        for matrix in set(partial_row_filled_states + partial_col_filled_states):
-            if matrix in memoized:
-                all_possible_states_winning_result.append(memoized[matrix])
+        partial_row_filled_states = [fill_partial_rows(new_inputs, m) for m in range(r)]
+        partial_col_filled_states = [fill_partial_cols(new_inputs, n) for n in range(c)]
+
+        partial_row_filled_states = list(filter(lambda x: x, partial_row_filled_states))
+        partial_col_filled_states = list(filter(lambda x: x, partial_col_filled_states))
+
+        print('r : {}, row : {}'.format(r, partial_row_filled_states))
+        print('c : {}, col : {}'.format(c, partial_col_filled_states))
+
+        winning_results = []
+        for matrix in partial_row_filled_states + partial_col_filled_states:
+            hashKey = hash(matrix, r, c)
+            if hashKey in memoized:
+                winning_results.append(memoized[hashKey])
                 continue
             result = check_is_winning(matrix)
-            memoized[matrix] = result
-            all_possible_states_winning_result.append(result)
+            memoized[hashKey] = result
+            winning_results.append(result)
 
-        return any(all_possible_states_winning_result)
+        return any(winning_results)
 
     for i in possible_rows:
         new_inputs = copy.deepcopy(inputs)
@@ -63,13 +83,13 @@ def solve(inputs, r, c):
     return win_counter
 
 
-def fill_partial_rows(inputs, i):
-    new_row = inputs[i]
-    tuple_new_row = list(enumerate(new_row))
+def fill_partial(line, is_row):
+    tuple_new_line = list(enumerate(line))
+    print('{} : {}'.format(i, tuple_new_line))
 
     list_of_tuples = []
     cur_tuples = []
-    for tuple in tuple_new_row:
+    for tuple in tuple_new_line:
         if tuple[1] == 'x':
             if cur_tuples:
                 list_of_tuples.append(cur_tuples)
@@ -80,45 +100,36 @@ def fill_partial_rows(inputs, i):
     if cur_tuples:
         list_of_tuples.append(cur_tuples)
 
-    partial_rows = [tuples for tuples in list_of_tuples if '#' not in map(itemgetter(1), tuples)]
+    print('{} : {}'.format(i, list_of_tuples))
+
+    partial_lines = [tuples for tuples in list_of_tuples if '#' not in map(lambda x: x[1], tuples)]
+
+    print(partial_lines)
 
     new_input_list = []
-    for partial_row in partial_rows:
+    for partial_line in partial_lines:
         new_inputs = copy.deepcopy(inputs)
-        for index, item in partial_row:
-            new_inputs[i][index] = 'x'
+        for index, item in partial_line:
+            if is_row:
+                new_inputs[i][index] = 'x'
+            else:
+                new_inputs[index][i] = 'x'
+        print('Input : {}'.format(new_inputs))
         new_input_list.append(new_inputs)
 
+    print('Input List : {}'.format(new_input_list))
+
     return new_input_list
+
+
+def fill_partial_rows(inputs, i):
+    new_row = inputs[i]
+    return fill_partial(new_row, True)
 
 
 def fill_partial_cols(inputs, i):
     new_col = inputs[:, i]
-    tuple_new_col = list(enumerate(new_col))
-
-    list_of_tuples = []
-    cur_tuples = []
-    for tuple in tuple_new_col:
-        if tuple[1] == 'x':
-            if cur_tuples:
-                list_of_tuples.append(cur_tuples)
-                cur_tuples = []
-            continue
-        cur_tuples.append(tuple)
-
-    if cur_tuples:
-        list_of_tuples.append(cur_tuples)
-
-    partial_cols = [tuples for tuples in list_of_tuples if '#' not in map(itemgetter(1), tuples)]
-
-    new_input_list = []
-    for partial_col in partial_cols:
-        new_inputs = copy.deepcopy(inputs)
-        for index, item in partial_col:
-            new_inputs[index][i] = 'x'
-        new_input_list.append(new_inputs)
-
-    return new_input_list
+    return fill_partial(new_col, False)
 
 
 def fill_row(inputs, i, j=0, k=None):
